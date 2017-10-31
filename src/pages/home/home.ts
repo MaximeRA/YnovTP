728fc593ee555eff55346adc495e5f7d97382105
Maxime Robeque <maxime.abdelouahedrobeque@ynov.com>
2017-10-31 16:54:37 +0100

    aficherfoto3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -39,7 +39,7 @@ export class HomePage {
     this.camera.getPicture(options).then((imageData) => {
     // imageData is either a base64 encoded string or a file URI
 // If it's base64:
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
       }, (err) => {
 // Handle error
       });
