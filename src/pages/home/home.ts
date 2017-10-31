e4dcaf5842f9176cbe0891ad13b0221db99b76d4
Maxime Robeque <maxime.abdelouahedrobeque@ynov.com>
2017-10-31 16:25:15 +0100

    trycamera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Camera } from '@ionic-native/camera';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -29,15 +29,19 @@ export class HomePage {
   }
 
   takePicture(){
-     this.app.camera.getPicture({
-         destinationType: this.app.camera.DestinationType.DATA_URL,
-         targetWidth: 1000,
-         targetHeight: 1000
-     }).then((imageData) => {
-       // imageData is a base64 encoded string
-         this.app.base64Image = "data:image/jpeg;base64," + imageData;
-     }, (err) => {
-         console.log(err);
-     });
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+}
+
+  this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+// If it's base64:
+   this.app.base64Image = 'data:image/jpeg;base64,' + imageData;
+}, (err) => {
+// Handle error
+});
    }
 }
